babfc8e25c2d47900213a5823888d3e49237d5d7
Kennethc203 <kennethc203@nycstudents.net>
2025-09-15 15:36:28 -0400

    Add files via upload

---
+++ DogWalkCompany.java
@@ -0,0 +1,14 @@
+public class DogWalkCompany {
+    private int dogsAvailable;
+    public int numAvailableDogs(int hour){
+        if (hour > 23 || hour < 0) return 0;
+        if (hour == 0) return
+    }
+
+    public DogWalkCompany(int doges){
+        dogsAvailable = doges;
+    }
+    public void updateDogs(int hour, int numberDogsWalked) {
+        numAvailableDogs(hour) -=
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        DogWalkCompany doug = new DogWalkCompany();
+        System.out.println(doug.numAvailableDogs(2));
+
+
+    }
+}
